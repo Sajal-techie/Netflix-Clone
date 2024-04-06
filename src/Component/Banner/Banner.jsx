9b458dbfe2a66cef12f1024d7810f04c695513de
Sajal K <sajalsaju687@gmail.com>
2024-04-06 10:44:59 +0530

    ui designed

---
+++ src/Component/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Banner.css'
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'> Movie Name</h1>
+            <div className="bannerbuttons">
+                <button className="buttton">play</button>
+                <button className="buttton">My list</button>
+            </div>
+            <h1 className="description">fasdf asjdf lksdjf jdf laskdjf  fljads fiajsdf aljdf alsdfj aisjdf adisj</h1>
+        </div> 
+      
+    </div>
+  )
+}
+
+export default Banner
